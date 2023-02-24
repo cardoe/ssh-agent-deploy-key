24e8b9231feca618ccfd1ff98eb4c58070c0df65
Doug Goldstein <cardoe@cardoe.com>
2023-02-23 23:01:09 -0600

    implement the actual action
    initial implementation of the actual action along with a README update
    to describe how it works along with updating the tests to run the action
    with some test keys.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,61 @@
 import * as core from '@actions/core';
+import * as ssh from './ssh';
+
+async function main(): Promise<void> {
+  try {
+    const privateKeyData = core.getInput('ssh-private-key', {
+      required: true,
+      trimWhitespace: true,
+    });
+    const privateKeys = ssh.parsePrivateKeys(privateKeyData);
+
+    const sshCmd = await ssh.createSshCmd();
+
+    core.startGroup('Starting ssh-agent');
+    await sshCmd.startAgent();
+    core.endGroup();
+
+    core.startGroup(`Loading ${privateKeys.length} private key(s)`);
+    await sshCmd.loadPrivateKeys(privateKeys);
+    core.endGroup();
+
+    core.startGroup('Configuring GitHub deploy keys');
+    const pubKeys = await sshCmd.listKeys();
+    core.info(`Got ${pubKeys.length} key(s) to check`);
+    const sshBasePath = await sshCmd.getDotSshPath();
+    core.info(`Using ${sshBasePath} for SSH key storage and config`);
+    const deployed = await ssh.configDeployKeys(sshBasePath, pubKeys);
+    core.info(`Configured ${deployed} key(s) to use as GitHub deploy keys`);
+    core.endGroup();
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    core.setFailed(`${(error as any)?.message ?? error}`);
+  }
+}
+
+async function cleanup(): Promise<void> {
+  try {
+    const sshCmd = await ssh.createSshCmd();
+
+    core.startGroup('Killing ssh-agent');
+    await sshCmd.killAgent();
+    core.endGroup();
+
+    core.startGroup('Cleaning up GitHub deploy keys');
+    await ssh.cleanupDeployKeys();
+    core.endGroup();
+  } catch (error) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    core.setFailed(`${(error as any)?.message ?? error}`);
+  }
+}
 
 async function run(): Promise<void> {
-  try {
-    core.info('got here');
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message);
+  if (core.getState('IS_POST')) {
+    cleanup();
+  } else {
+    core.saveState('IS_POST', true);
+    main();
   }
 }
 
